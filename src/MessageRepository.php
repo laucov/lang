9b6ce34cb0dd89afce94b3349e16f1528c0c3edd
covaleski <covaleski23@gmail.com>
2024-02-29 10:46:21 -0300

    feat: create `MessageRepository`

---
+++ src/MessageRepository.php
@@ -0,0 +1,174 @@
+<?php
+
+/**
+ * This file is part of Laucov's Language Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package lang
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Lang;
+
+use Laucov\Arrays\ArrayBuilder;
+
+/**
+ * Stores and retrieves multi-language messages.
+ */
+class MessageRepository
+{
+    /**
+     * Default language.
+     */
+    public null|string $defaultLanguage = null;
+
+    /**
+     * Whether to use all accepted languages to find a message.
+     */
+    public bool $fallback = true;
+
+    /**
+     * Accepted languages.
+     * 
+     * @var array<string>
+     */
+    protected array $accepted = [];
+
+    /**
+     * Language data.
+     * 
+     * @var array<string, ArrayBuilder>
+     */
+    protected array $data = [];
+
+    /**
+     * Language file directories.
+     * 
+     * @var array<string>
+     */
+    protected array $directories = [];
+
+    /**
+     * Supported languages.
+     * 
+     * @var array<string>
+     */
+    protected array $supported = [];
+
+    /**
+     * Set a language data source directory.
+     * 
+     * PHP files in the directory will be used to get language data on demand.
+     */
+    public function addDirectory(string $path): static
+    {
+        $this->directories[] = rtrim($path, '\\/');
+        return $this;
+    }
+
+    /**
+     * Find and format a message.
+     */
+    public function findMessage(string $path, array $args = []): null|string
+    {
+        // Split path.
+        $segments = explode('.', $path);
+
+        // Get message from accepted languages.
+        $message = null;
+        foreach ($this->accepted as $tag) {
+            // Check if is supported.
+            if (!in_array($tag, $this->supported, true)) {
+                continue;
+            }
+            // Check if exists and try to load data.
+            if (!isset($this->data[$tag]) && !$this->loadLanguageData($tag)) {
+                continue;
+            }
+            // Get message.
+            $message = $this->data[$tag]->getValue($segments);
+            if ($message !== null || !$this->fallback) {
+                break;
+            }
+        }
+
+        // Check default language.
+        if ($message === null && $this->defaultLanguage !== null) {
+            $tag = $this->defaultLanguage;
+            if (isset($this->data[$tag]) || $this->loadLanguageData($tag)) {
+                $message = $this->data[$tag]->getValue($segments);
+            }
+        }
+
+        // Format.
+        if ($message !== null && count($args) > 0) {
+            $message = msgfmt_format_message($tag, $message, $args);
+        }
+
+        return $message;
+    }
+
+    /**
+     * Set accepted languages.
+     */
+    public function setAcceptedLanguages(LanguageList $list): static
+    {
+        $this->accepted = $list->getTags();
+        return $this;
+    }
+
+    /**
+     * Set messages for the given language.
+     */
+    public function setLanguageData(string $tag, array $data): static
+    {
+        $this->data[$tag] = new ArrayBuilder($data);
+        return $this;
+    }
+
+    /**
+     * Set supported language tags.
+     */
+    public function setSupportedLanguages(string ...$tags): static
+    {
+        $this->supported = $tags;
+        return $this;
+    }
+
+    /**
+     * Search directories and try to load language data for the given locale.
+     */
+    protected function loadLanguageData(string $locale): bool
+    {
+        // Check each registered directory.
+        foreach ($this->directories as $directory) {
+            // Build filename.
+            $filename = $directory . DIRECTORY_SEPARATOR . $locale . '.php';
+            // Check if exists and require the script.
+            if (file_exists($filename)) {
+                $this->setLanguageData($locale, require $filename);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/MessageRepositoryTest.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * This file is part of Laucov's Language Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package lang
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Laucov\Arrays\ArrayBuilder;
+use Laucov\Lang\LanguageList;
+use Laucov\Lang\MessageRepository;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Lang\MessageRepository
+ */
+class MessageRepositoryTest extends TestCase
+{
+    /**
+     * @covers ::addDirectory
+     * @covers ::findMessage
+     * @covers ::loadLanguageData
+     * @covers ::setAcceptedLanguages
+     * @covers ::setLanguageData
+     * @covers ::setSupportedLanguages
+     * @uses Laucov\Lang\Language::__construct
+     * @uses Laucov\Lang\LanguageList::add
+     * @uses Laucov\Lang\LanguageList::compare
+     * @uses Laucov\Lang\LanguageList::fromHeader
+     * @uses Laucov\Lang\LanguageList::get
+     * @uses Laucov\Lang\LanguageList::getTags
+     */
+    public function testCanAddLanguagesAndFindMessages(): void
+    {
+        // Create archive.
+        $archive = new MessageRepository();
+
+        // Create a list of accepted languages.
+        $list = LanguageList::fromHeader('pt-PT;q=0.5, pt-BR;q=0.9');
+
+        // Configure.
+        $archive->defaultLanguage = 'en-US';
+        $archive
+            ->setAcceptedLanguages($list)
+            ->setSupportedLanguages('pt-PT', 'pt-BR', 'en')
+            ->setLanguageData('en-US', [
+                'fox' => 'The quick brown fox jumps over the lazy dog.',
+                'hello' => [
+                    'world' => 'Hello, World!',
+                    'universe' => 'Hello, Universe!',
+                    'everyone' => 'Hello, Everyone!',
+                ],
+                'count' => '{0,number,integer} files found',
+            ])
+            ->setLanguageData('pt-BR', [
+                'today' => 'Hoje é {date, date, full}.',
+                'fox' => 'A raposa marrom e ligeira pula sobre o cachorro preguiçoso.',
+                'hello' => [
+                    'world' => 'Olá, Mundo!',
+                ],
+                'count' => '{0,number,integer} arquivos encontrados',
+            ])
+            ->setLanguageData('pt-PT', [
+                'fox' => 'A rápida raposa marrom salta sobre o cão preguiçoso.',
+                'hello' => [
+                    'universe' => 'Olá, Universo!',
+                ],
+                'count' => '{0,number,integer} ficheiros encontrados',
+            ]);
+        
+        // Get a message.
+        $message_a = $archive->findMessage('fox');
+        $this->assertSame(
+            'A raposa marrom e ligeira pula sobre o cachorro preguiçoso.',
+            $message_a,
+        );
+
+        // Test nesting.
+        $message_b = $archive->findMessage('hello.world');
+        $this->assertSame('Olá, Mundo!', $message_b);
+
+        // Test fallback to other accepted languages.
+        $message_c = $archive->findMessage('hello.universe');
+        $this->assertSame('Olá, Universo!', $message_c);
+        
+        // Test fallback to the default language.
+        $message_d = $archive->findMessage('hello.everyone');
+        $this->assertSame('Hello, Everyone!', $message_d);
+        $archive->defaultLanguage = null;
+        $this->assertNull($archive->findMessage('hello.everyone'));
+
+        // Test without fallback.
+        $archive->fallback = false;
+        $archive->defaultLanguage = 'en-US';
+        $message_e = $archive->findMessage('hello.universe');
+        $this->assertSame('Hello, Universe!', $message_e);
+        $archive->defaultLanguage = null;
+        $this->assertNull($archive->findMessage('hello.universe'));
+
+        // Test formatting.
+        $message_f = $archive->findMessage('today', ['date' => 0]);
+        $this->assertSame(
+            'Hoje é quinta-feira, 1 de janeiro de 1970.',
+            $message_f,
+        );
+
+        // Limit supported languages.
+        $message_g = $archive
+            ->setSupportedLanguages('pt-PT', 'es')
+            ->findMessage('count', [2141]);
+        $this->assertSame("2\u{00A0}141 ficheiros encontrados", $message_g);
+        $message_h = $archive
+            ->setSupportedLanguages('es')
+            ->findMessage('count', [2141]);
+        $this->assertNull($message_h);
+
+        // Always support the default language.
+        $archive->defaultLanguage = 'en-US';
+        $message_i = $archive->findMessage('count', [2141]);
+        $this->assertSame("2,141 files found", $message_i);
+
+        // Prepare for data caching tests.
+        $property = (new \ReflectionObject($archive))->getProperty('data');
+        $archive->fallback = true;
+
+        // Add directories.
+        $archive
+            ->addDirectory(__DIR__ . '/lang-files-a/')
+            ->addDirectory(__DIR__ . '/lang-files-b')
+            ->setSupportedLanguages('fr-FR', 'es')
+            ->setAcceptedLanguages(LanguageList::fromHeader('es, fr-FR'));
+        $data = $property->getValue($archive);
+        $this->assertArrayNotHasKey('es', $data);
+        $this->assertArrayNotHasKey('fr-FR', $data);
+
+        // Get spanish message.
+        $this->assertSame(
+            'El veloz zorro marrón salta sobre el perro perezoso.',
+            $archive->findMessage('fox'),
+        );
+        $data = $property->getValue($archive);
+        $this->assertArrayHasKey('es', $data);
+        $this->assertArrayNotHasKey('fr-FR', $data);
+    }
+}
